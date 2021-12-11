a750653d695a7f7fdf740ceee16d2adffd874a7c
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2021-12-12 01:48:40 +0900

    Chore: deploy to beanstalk

---
+++ blood-donation/src/main/java/com/gachonsw/blooddonation/controller/RecommendController.java
@@ -0,0 +1,16 @@
+//package com.gachonsw.blooddonation.controller;
+//
+//import com.gachonsw.blooddonation.dto.Result;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/recommend")
+//public class RecommendController {
+//
+//    @GetMapping("/userId")
+//    public Result<?> recommend(@RequestParam Long userId){
+//
+//    }
+//}
